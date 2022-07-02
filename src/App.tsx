88e214df518ddde2d6ead741e4bc04da20903305
Zaid-maker <pzhafeez@gmail.com>
2022-07-02 22:59:15 +0500

    done so far

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
+import { Route, Routes } from "react-router-dom";
+import { Container } from "react-bootstrap";
+
+// Import pages
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Store } from "./pages/Store";
+
+// Import components
+import { Navbar } from "./components/Navbar";
+import { ShoppingCartProvider } from "./context/ShoppingCartContext";
+
 function App() {
-  return <h1>Hi</h1>;
+  return (
+    <ShoppingCartProvider>
+      <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/store" element={<Store />} />
+        </Routes>
+      </Container>
+    </ShoppingCartProvider>
+  );
 }
 
 export default App;
